748fcb25ea0cac46678b416d568412f8ed9b4688
Abhishek Bachhav <113822819+abhishekbachhav@users.noreply.github.com>
2022-11-02 18:43:45 +0530

    Add files via upload

---
+++ MultiOfNum.java
@@ -0,0 +1,9 @@
+public class MultiOfNum {
+    public static void main(String[] args) {
+        int a=10;
+        int b=3;
+        int result=a*b;
+        System.out.println(result);
+    }
+    
+}
---
+++ SubOfNum.java
@@ -0,0 +1,12 @@
+public class SubOfNum{
+    /**
+     * @param args
+     */
+    public static void main(Strings[]args) {
+        int a=10;
+        int b=20;
+        int result=a-b;
+        System.out.println(result);
+        
+    }
+}
